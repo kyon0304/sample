713a50455e5d79f47b613431bcac3ec2424507c9
kyon <kyon0304@gmail.com>
2015-01-20 23:10:39 +0800

    use helper function full_title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,11 +6,19 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector( 'h1', :text=>"Sample App" )
-      page.should have_selector( 'title', 
-                              :text=>"#{base_title} | Home" )
+    end
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should have a custom page title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => '| Home')
     end
   end
 
